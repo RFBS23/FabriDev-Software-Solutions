6fdb56141b12baaa48b036423d402993ec44151e
RFBS18 <fabriziobarrios22@gmail.com>
2024-12-30 01:06:58 -0500

    actualizando not foud

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,6 @@
 'use client';
 import './not-found.css';
-import './bootstrap.bundle.min.js';
+import './bootstrap.css';
 
 export default function NotFound() {
     return (
@@ -11,9 +11,6 @@ export default function NotFound() {
             <link
                 href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
                 rel="stylesheet"/>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-                  integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-                  crossOrigin="anonymous"/>
         </head>
         <body style={{userSelect: "none", background: "#f6f9ff"}}>
         <main>
